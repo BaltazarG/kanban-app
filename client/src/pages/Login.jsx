6ae55267c740ad050dcbc760471a12de2d9441c5
BaltazarG <b-garciacanllo@hotmail.com>
2022-07-21 19:26:45 -0300

    isError?

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -40,7 +40,7 @@ const Login = () => {
       localStorage.setItem("token", res.token);
       navigate("/");
     } catch (err) {
-      const errors = err.data.errors;
+      const errors = err?.data.errors;
       errors?.forEach(e => {
         if (e.param === "username") {
           setUsernameErrText(e.msg);
